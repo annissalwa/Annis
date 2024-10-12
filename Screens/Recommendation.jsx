4df948d47b99c63eab57ebf6e98ac27340593f69
annissalwa <d20221101828@siswa.upsi.edu.my>
2024-10-12 16:47:13 +0800

    Fourth Commit

--- Screens/Recommendation.jsx
+++ Screens/Recommendation.jsx
@@ -6,7 +6,7 @@ import Slideshow from 'react-native-image-slider-show'
 export class Recommendation extends Component {
     render() {
         return (
-            <View className="flex-1 items-center py-8 -top-9">
+            <View className="flex-1 items-center justify-start py-10 -top-9">
                 <ScrollView className="-mx-10 pl-2">
                     <View className="w-96 h-16 rounded-lg bg-emerald-400 top-2 left-10">
                         <View className="flex-row">
@@ -16,7 +16,7 @@ export class Recommendation extends Component {
                     </View>
                     <View className="w-full">
                         <View>
-                            <Image className="w-full h-60 top-3"  source={require("../assets/main recommendation.jpg")}></Image>
+                            <Image className="w-full h-60 top-3" source={require("../assets/main recommendation.jpg")}></Image>
                             <Text className="text-5xl text-white font-extrabold -top-36 text-center" >What is Today's Exploring</Text>
                         </View>
                     </View>
@@ -26,20 +26,68 @@ export class Recommendation extends Component {
                             <Text className="text-base text-neutral-500 -top-4">Search by city, trail, park</Text>
                         </View>
                     </View>
-                    <View className="items-center -top-14">
+                    <View className="items-start ml-44 left-2 -top-14">
                         <Text className="text-2xl font-extrabold right-32">Near Me</Text>
                     </View>
-                    <View className="w-90 h-90 bg-black -top-11">
+                    <View className="w-90 h-90 -top-11">
                         <Slideshow
                             dataSource={[
-                                { url: require("../assets/main recommendation.jpg") },
-                                { url: require("../assets/main recommendation.jpg") },
-                                { url: require("../assets/main recommendation.jpg") }
+                                { url: require("../assets/r broga hill.jpg") },
+                                { url: require("../assets/r mount alai.jpg") },
+                                { url: require("../assets/r mount stong.jpg") }
                             ]} />
-                        <Text className="from-neutral-50 top-10 bg-black">Let's go Hiking</Text>
+                        <Text className="text-black top-10 left-10">Let's go Hiking</Text>
                     </View>
-                    <View className="items-center top-56">
-                        <Image className="w-full h-60 -top-15 rounded-3xl right-5 bg-black " resizeMode="contain" source={require("../assets/main recommendation.jpg")}></Image>
+                    <View>
+                        <View className="items-start ml-44 left-2 top-14">
+                            <Text className="text-2xl font-extrabold right-32">Popular</Text>
+                        </View>
+                        <View className="items-center top-14">
+                            <Image className="w-80 h-52 top-3 rounded-2xl right-2" resizeMode="full" source={require("../assets/r mount kinabalu.jpg")}></Image>
+                            <Text className="top-5 right-28 text-lg font-semibold">Mount Kinabalu</Text>
+                            <Text className="top-5 right-28 text-sm font-normal mr-8 underline ">Ranau, Sabah</Text>
+                            <View className="flex-row">
+                                <Image className="w-4 h-4 right-9 top-6 mr-6" resizeMode="full" source={require("../assets/icon star.png")}></Image>
+                                <Text className="top-6 right-12 text-sm font-normal ">4.9 (2138) . Hard . 22.4km . Est.187days</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View>
+                        <View className="items-center top-28">
+                            <Image className="w-80 h-52 top-3 rounded-2xl right-2" resizeMode="full" source={require("../assets/r penang hill.jpg")}></Image>
+                            <Text className="top-5 right-24 text-lg font-semibold">Penang Hill Heritage</Text>
+                            <Text className="top-5 right-28 text-sm font-normal mr-12 underline ">Penang Hill</Text>
+                            <View className="flex-row right-3">
+                                <Image className="w-4 h-4 right-9 top-6 mr-6" resizeMode="full" source={require("../assets/icon star.png")}></Image>
+                                <Text className="top-6 right-12 text-sm font-normal ">4.7 (145) . Hard . 5.1km . Est.3h 19m</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View>
+                        <View className="items-start ml-44 left-2 top-44">
+                            <Text className="text-2xl font-extrabold right-32 mr-7">State</Text>
+                        </View>
+                        <View className="flex-row items-start ml-14">
+                            <View>
+                                <Image className="w-32 h-32 top-48 rounded-2xl " resizeMode="full" source={require("../assets/r mount kinabalu.jpg")}></Image>
+                                <Text className="font-bold text-3xl text-white top-28 left-8">Perak</Text>
+                            </View>
+                            <View>
+                                <Image className="w-32 h-32 top-48 rounded-2xl ml-3" resizeMode="full" source={require("../assets/r mount kinabalu.jpg")}></Image>
+                                <Text className="font-bold text-3xl text-white top-28 left-10">Perak</Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View>
+                        <View className="items-center top-full">
+                            <Image className="w-80 h-52 top-full rounded-2xl right-2" resizeMode="full" source={require("../assets/r mount kinabalu.jpg")}></Image>
+                            <Text className="top-5 right-28 text-lg font-semibold">Mount Kinabalu</Text>
+                            <Text className="top-5 right-28 text-sm font-normal mr-8 underline ">Ranau, Sabah</Text>
+                            <View className="flex-row">
+                                <Image className="w-4 h-4 right-9 top-6 mr-6" resizeMode="full" source={require("../assets/icon star.png")}></Image>
+                                <Text className="top-6 right-12 text-sm font-normal ">4.9 (2138) . Hard . 22.4km . Est.187days</Text>
+                            </View>
+                        </View>
                     </View>
 
 
--- Screens/home.jsx
+++ Screens/home.jsx
@@ -6,63 +6,88 @@ export class Home extends Component {
   render() {
     return (
       <View className="flex-1 items-center justify-start py-8 -top-9">
-      <ScrollView className="-mx-10 pl-12">
-        <View className="w-96 h-16 rounded-lg bg-emerald-400 top-2 -left-3">
-          <View className="flex-row">
-            <Image className="w-10 h-10 top-3 left-3 rounded-full " source={require("../assets/Hiking-icon.jpg")}></Image>
-            <Text className="text-left top-4 text-3xl font-semibold left-5">HikeMate</Text>
+        <ScrollView className="-mx-10 pl-12">
+          <View className="w-96 h-16 rounded-lg bg-emerald-400 top-2 -left-3">
+            <View className="flex-row">
+              <Image className="w-10 h-10 top-3 left-3 rounded-full " source={require("../assets/Hiking-icon.jpg")}></Image>
+              <Text className="text-left top-4 text-3xl font-semibold left-5">HikeMate</Text>
+            </View>
           </View>
-        </View>
-        <View className="w-screen">
-          <Image className="w-full h-60 top-3 right-5" resizeMode="contain" source={require("../assets/Hiking Forest.jpg")}></Image>
-        </View>
-        <View>
-          <Text className="text-left text-3xl top-10 font-medium">Activity</Text>
-        </View>
-        <View className="flex-row">
-          <View className="mr-1 w-20 h-20 rounded-lg bg-green-300 top-12">
-            <Pressable onPress={() => Alert.alert('Pressed')}>
-              <Image className="w-10 h-10 top-5 left-5 " source={require("../assets/icon hiking.png")}></Image>
-            </Pressable>
+          <View className="w-screen">
+            <Image className="w-full h-60 -top-1 right-5" resizeMode="contain" source={require("../assets/promote.jpg")}></Image>
           </View>
-          <View className="mr-1 w-20 h-20 rounded-lg bg-green-300 top-12">
-            <Pressable onPress={() => Alert.alert('Pressed')}>
-              <Image className="w-10 h-10 top-5 left-5 " source={require("../assets/icon campsite.png")}></Image>
-            </Pressable>
+          <View className="-top-6">
+            <View>
+              <Text className="text-left text-3xl top-10 font-medium">Activity</Text>
+            </View>
+            <View className="flex-row">
+              <View className="mr-1 w-20 h-20 rounded-lg bg-green-300 top-12">
+                <Pressable onPress={() => Alert.alert('Pressed')}>
+                  <Image className="w-10 h-10 top-5 left-5 " source={require("../assets/icon hiking.png")}></Image>
+                </Pressable>
+              </View>
+              <View className="mr-1 w-20 h-20 rounded-lg bg-green-300 top-12">
+                <Pressable onPress={() => Alert.alert('Pressed')}>
+                  <Image className="w-10 h-10 top-5 left-5 " source={require("../assets/icon campsite.png")}></Image>
+                </Pressable>
+              </View>
+              <View className="mr-1 w-20 h-20 rounded-lg bg-green-300 top-12">
+                <Pressable onPress={() => Alert.alert('Pressed')}>
+                  <Image className="w-10 h-10 top-5 left-5 " source={require("../assets/icon trails.png")}></Image>
+                </Pressable>
+              </View>
+              <View className="mr-1 w-20 h-20 rounded-lg bg-green-300 top-12">
+                <Pressable onPress={() => Alert.alert('Pressed')}>
+                  <Image className="w-10 h-10 top-5 left-5 " source={require("../assets/icon outdoor.png")}></Image>
+                </Pressable>
+              </View>
+            </View>
+            <View className="flex-row top-14 right-3">
+              <Text className="text-center mr-2 w-24 font-medium">Hike</Text>
+              <Text className="text-center mr-2 w-16 font-medium">Campsite</Text>
+              <Text className="text-center mr-2 w-20 font-medium">Trails</Text>
+              <Text className="text-center mr-2 w-20 font-medium">Outdoor Activities</Text>
+            </View>
           </View>
-          <View className="mr-1 w-20 h-20 rounded-lg bg-green-300 top-12">
-            <Pressable onPress={() => Alert.alert('Pressed')}>
-              <Image className="w-10 h-10 top-5 left-5 " source={require("../assets/icon trails.png")}></Image>
-              </Pressable>
+          <View className="top-2">
+            <View className="justify-start left-14 top-2">
+              <Text className="text-left text-3xl top-14 right-14 font-medium">Review</Text>
+            </View>
+            <View>
+              <View className="justify-start w-60 left-20 top-14">
+                <Image className="w-14 h-14 rounded-full right-20 top-5" source={require("../assets/lisa racher.jpg")}></Image>
+                <Text className="font-semibold text-base right-3 -top-9">Lisa Racher</Text>
+                <Text className="font-sm right-3 -top-8 text-gray-500">Mount Kinabalu is one of the most popular hiking destination in Sabah</Text>
+              </View>
+              <View>
+                <Image className="w-44 h-52 top-11 rounded-xl" source={require("../assets/Hiking Forest.jpg")}></Image>
+                <Image className="w-40 h-24 -top-40 left-44 ml-1 rounded-xl" source={require("../assets/Hiking Forest.jpg")}></Image>
+                <Image className="w-40 h-24 -top-36 left-44 ml-1 rounded-xl" source={require("../assets/Hiking Forest.jpg")}></Image>
+              </View>
+              <View className="items-start flex-row -top-36">
+                <Image className="w-8 h-8" resizeMode="contain" source={require("../assets/love icon.png")}></Image>
+                <Text className="top-2 left-3 text-base">956 people liked this</Text>
+              </View>
+            </View>
+            <View className="-top-44">
+              <View className="justify-start w-60 left-20 top-14">
+                <Image className="w-14 h-14 rounded-full right-20 top-5" source={require("../assets/richard harllow.jpg")}></Image>
+                <Text className="font-semibold text-base right-3 -top-9">Richard Harllow</Text>
+                <Text className="font-sm right-3 w-64 -top-8 text-gray-500">Bukit Broga is a fantastic hiking spot with well-mountained trails and stunning ssunrise views from the summit. It's perfect for all skill levels, just remember to wear good shoes and bring water!</Text>
+              </View>
+              <View>
+                <Image className="w-44 h-52 top-11 rounded-xl" source={require("../assets/richard r 1.jpg")}></Image>
+                <Image className="w-40 h-24 -top-40  left-44 ml-1 rounded-xl" source={require("../assets/richard r 2.jpg")}></Image>
+                <Image className="w-40 h-24 -top-36 left-44 ml-1 rounded-xl" source={require("../assets/richard r 3.jpg")}></Image>
+              </View>
+              <View className="items-start flex-row -top-36">
+                <Image className="w-8 h-8" resizeMode="contain" source={require("../assets/love icon.png")}></Image>
+                <Text className="top-2 left-3 text-base">256 people liked this</Text>
+              </View>
+            </View>
           </View>
-          <View className="mr-1 w-20 h-20 rounded-lg bg-green-300 top-12">
-            <Pressable onPress={() => Alert.alert('Pressed')}>
-              <Image className="w-10 h-10 top-5 left-5 " source={require("../assets/icon outdoor.png")}></Image>
-            </Pressable>
-          </View>
-        </View>
-        <View className="flex-row top-14 right-3">
-          <Text className="text-center mr-2 w-24 font-medium">Hike</Text>
-          <Text className="text-center mr-2 w-16 font-medium">Campsite</Text>
-          <Text className="text-center mr-2 w-20 font-medium">Trails</Text>
-          <Text className="text-center mr-2 w-20 font-medium">Outdoor Activities</Text>
-        </View>
-        <View className="justify-start left-14 top-2">
-          <Text className="text-left text-3xl top-14 right-14 font-medium">Review</Text>
-        </View>
-        <View className="justify-start w-60 left-20 top-14">
-          <Image className="w-14 h-14 rounded-full right-20 top-5" source={require("../assets/Hiking Forest.jpg")}></Image>
-          <Text className="font-semibold text-base right-3 -top-9">Lisa Racher</Text>
-          <Text className="font-sm right-3 -top-8 text-gray-500">Mount Kinabalu is one of the most popular hiking destination in Sabah</Text>
-        </View>
-        <View>
-          <Image className="w-60 h-44 top-11 rounded-xl" source={require("../assets/Hiking Forest.jpg")}></Image>
-          <Image className="w-24 h-20 -top-32  left-60 ml-1 rounded-xl" source={require("../assets/Hiking Forest.jpg")}></Image>
-          <Image className="w-24 h-20 -top-28 left-60 ml-1 rounded-xl" source={require("../assets/Hiking Forest.jpg")}></Image>
-        </View>
-        
-      </ScrollView>
-    </View>
+        </ScrollView>
+      </View>
     )
   }
 }
